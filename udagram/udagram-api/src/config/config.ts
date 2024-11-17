fc7a69074f878103eb3207042585ad999d3188a4
TrungPD <trungkaka310@gmail.com>
2024-11-17 16:49:10 +0700

    upd6

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -6,30 +6,30 @@ dotenv.config();
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
 
-export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL,
-  jwt: {
-    secret: process.env.JWT_SECRET,
-  },
-};
-
 // export const config = {
-//   username: `postgres`,
-//   password: `postgres`,
-//   database: `postgres`,
-//   host: 'mydb.ciql77kn1xkx.us-east-1.rds.amazonaws.com',
-//   aws_region: 'us-east-1',
-//   aws_profile: 'default',
-//   aws_media_bucket: 'arn:aws:s3:::mybucket11152024',
-//   url: 'http://localhost:8100',
+//   username: `${process.env.POSTGRES_USERNAME}`,
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: process.env.POSTGRES_DB,
+//   host: process.env.POSTGRES_HOST,
+//   aws_region: process.env.AWS_REGION,
+//   aws_profile: process.env.AWS_PROFILE,
+//   aws_media_bucket: process.env.AWS_BUCKET,
+//   url: process.env.URL,
 //   jwt: {
-//     secret: 'mysecretstring',
+//     secret: process.env.JWT_SECRET,
 //   },
 // };
+
+export const config = {
+  username: `postgres`,
+  password: `postgres`,
+  database: `postgres`,
+  host: 'mydb.ciql77kn1xkx.us-east-1.rds.amazonaws.com',
+  aws_region: 'us-east-1',
+  aws_profile: 'default',
+  aws_media_bucket: 'arn:aws:s3:::mybucket11152024',
+  url: 'http://localhost:8100',
+  jwt: {
+    secret: 'mysecretstring',
+  },
+};
